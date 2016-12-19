38262ff4aeee8378e8b62f51d8190121171c1dcc
Rafael La Buonora <rlabuonora@yahoo.com>
2016-12-19 14:46:23 -0300

    Add raect-boostrap collapsible panel component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import { Button, Panel } from 'react-bootstrap';
 import './App.css';
 
+
+class Recipe extends Component {
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            open: false
+        };
+    }
+    render() {
+        return(
+            <div className="recipe">
+            <Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+            click
+        </Button>
+            <Panel collapsible expanded={this.state.open}>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+            Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </Panel>
+                </div>
+        );
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
-    );
-  }
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            open: true
+        };
+    }
+
+    render() {
+        return (
+                <div className="container">
+                <h1>Recipe Box </h1>
+                  <div className="well">
+                    <Recipe />
+                    <Recipe />
+                  </div>
+                </div>
+        );
+    }
 }
 
 export default App;
