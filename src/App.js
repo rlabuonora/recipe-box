4f4c15311391344fc4e5f90e4a2ec322a221f472
Rafael La Buonora <rlabuonora@yahoo.com>
2016-12-19 15:52:54 -0300

    Add Modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react';
-import { Button, Panel } from 'react-bootstrap';
+import { Button, Panel, Modal } from 'react-bootstrap';
 import './App.css';
 
+
+
+class AddButton extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        }
+    }
+
+    close() {
+        this.setState({ showModal: false });
+    }
+
+    open() {
+        this.setState({ showModal: true });
+    }
+
+    render() {
+        return(
+                <div>
+                <Button bsStyle="primary"
+                        onClick={this.open.bind(this)}
+                >
+                  Add Recipe
+            </Button>
+                <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <h4> Text in modal </h4>
+                </Modal.Body>
+
+                  <Modal.Footer>
+                  <Button onClick={this.close.bind(this)}>Close</Button>
+                  </Modal.Footer>
+              </Modal>
+                
+                </div>
+        );
+    }
+}
 
 class Recipe extends Component {
     constructor(...args) {
@@ -14,11 +57,18 @@ class Recipe extends Component {
         return(
             <div className="recipe">
             <Button onClick={ ()=> this.setState({ open: !this.state.open })}>
-            click
-        </Button>
-            <Panel collapsible expanded={this.state.open}>
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-            Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                Spaghetti
+            </Button>
+              <Panel collapsible expanded={this.state.open}>
+                <h3> Ingredients </h3>            
+              <ul className="list-group">
+               <li className="list-group-item">Cras justo odio</li>
+                <li className="list-group-item">Dapibus ac facilisis in</li>
+                <li className="list-group-item">Morbi leo risus</li>
+                <li className="list-group-item">Porta ac consectetur ac</li>
+                </ul>
+                <Button bsStyle="warning">Edit </Button>
+                <Button bsStyle="danger">Delete </Button>
             </Panel>
                 </div>
         );
@@ -26,13 +76,6 @@ class Recipe extends Component {
 }
 
 class App extends Component {
-    constructor(...args) {
-        super(...args);
-        this.state = {
-            open: true
-        };
-    }
-
     render() {
         return (
                 <div className="container">
@@ -40,6 +83,7 @@ class App extends Component {
                   <div className="well">
                     <Recipe />
                     <Recipe />
+                    <AddButton />
                   </div>
                 </div>
         );
